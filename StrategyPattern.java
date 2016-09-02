37513df92ab562fd2f23162061992ef9f650baca
DongyangY <dongyang.yao@rutgers.edu>
2016-09-02 23:56:12 +0800

    add strategy

---
+++ StrategyPattern.java
@@ -0,0 +1,46 @@
+/*
+ * The Strategy Pattern defines a family of algorithms, encapsulates each one,
+ * and make them interchangeable. Strategy lets the algorithm vary independently
+ * from clients that use it.
+ */
+
+public class StrategyPattern {
+    MutableDuck md = new MutableDuck();
+    md.performFly();
+    md.setFlyBehavior(new FlyNoWay());
+    md.performFly();
+}
+
+abstract class Duck {
+    FlyBehavior flyBehavior;
+
+    public void performFly() {
+	flyBehavior.fly();
+    }
+
+    public void setFlyBehavior(FlyBehavior fb) {
+	flyBehavior = fb;
+    }
+}
+
+class MutableDuck {
+    public MutableDuck() {
+	flyBehavior = new FlyWithWings();
+    }
+}
+
+interface FlyBehavior {
+    public void fly();
+}
+
+class FlyWithWings implements FlyBehavior {
+    public void fly() {
+	System.out.println("I'm flying!");
+    }
+}
+
+class FlyNoWay implements FlyBehavior {
+    public void fly() {
+	System.out.println("I can't fly!");
+    }
+}
