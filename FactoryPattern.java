95956c9fbe309c764c3d7b3d6e36354abe671c36
DongyangY <dongyang.yao@rutgers.edu>
2016-09-06 13:29:54 +0800

    add factory pattern

--- FactoryPattern.java
+++ FactoryPattern.java
@@ -1,3 +1,9 @@
+/*
+ * The Factory Method Pattern defines an interface for creating an object,
+ * but lets subclasses decide which class to instantiate. Factory Method
+ * lets a class defer instantiation to subclasses.
+ */
+
 abstract class PizzaStore {
     public Pizza orderPizza(String type) {
 	Pizza pizza;
